64d10b9d6125d5676bff094cebc243f4b237307e
aayush <hiaayush30@gmail.com>
2025-05-12 17:28:21 +0530

    added cv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,17 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div className='flex justify-center'>
+    <div id='home' className='flex justify-center'>
       <main className="md:w-[70%] sm:w-[90%] min-h-screen  text-white px-6 py-12 space-y-24">
         <Hero />
         <About />
-        <TechStack/>
-        <Projects/>
-        <Contact/>
+        <TechStack />
+        <Projects />
+        <Contact />
         <NavBar />
       </main>
+      <a target='_blank' href='https://drive.google.com/file/d/1H3wdaBGV8zDgqSCYmyl6bToVl8zgIYyJ/view?usp=sharing'
+        className='size-10 hover:scale-110 bg-cyan-500 fixed top-5 left-5 rounded-full text-white flex items-center justify-center font-semibold'>CV</a>
     </div>
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,47 +1,90 @@
 "use client"
-import { motion } from "motion/react"
-import React from 'react'
+import { motion } from "framer-motion"
+import React, { useState } from 'react'
+import { text } from "stream/consumers";
+
+
+const links = [
+  {
+    text: "/",
+    href: "#home"
+  },
+  {
+    text: "About Me",
+    href: "#about"
+  },
+  {
+    text: "Tech Stack",
+    href: "#techStack"
+  },
+  {
+    text: "Projects",
+    href: "#projects"
+  },
+  {
+    text: "Blogs",
+    href: "#blogs"
+  },
+  {
+    text: "Contact",
+    href: "#contact"
+  }
+]
+
+const containerVariants = {
+  hidden: { opacity: 0, right: -20 },
+  visible: {
+    opacity: 100,
+    right: 10,
+    transition: {
+      delay: 2.7,
+      ease: 'easeInOut',
+      duration: 1,
+      staggerChildren: 0.5, // Stagger the children's animation
+    }
+  }
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, x: 20 },
+  visible: { opacity: 1, x: 0 }
+};
 
 function NavBar() {
+  const [hovered, setHovered] = useState<number | null>(null);
   return (
     <motion.nav
-      initial={{
-        opacity: 0,
-        right:-20
-      }}
-      animate={{
-        opacity:100,
-        right:10
-      }}
-      transition={{
-        delay:2.7,
-        ease:'easeInOut',
-        duration:1
-      }}
-      className='lg:text-lg xl:text-xl hidden md:flex fixed right-5 top-1/2 -translate-y-1/2 flex-col space-y-10'>
-      <button onClick={() => {
-        document.querySelector('#about')?.scrollIntoView({ behavior: "smooth" })
-      }}
-        className='cursor-pointer hover:scale-105 hover:text-cyan-400 transition-all'>About Me</button>
-      <button onClick={() => {
-        document.querySelector('#techStack')?.scrollIntoView({ behavior: "smooth" })
-      }}
-        className='cursor-pointer hover:scale-105 hover:text-cyan-400 transition-all'>Tech Stack</button>
-      <button onClick={() => {
-        document.querySelector('#projects')?.scrollIntoView({ behavior: "smooth" })
-      }}
-        className='cursor-pointer hover:scale-105 hover:text-cyan-400 transition-all'>Projects</button>
-      <button onClick={() => {
-        document.querySelector('#blogs')?.scrollIntoView({ behavior: "smooth" })
-      }}
-        className='cursor-pointer hover:scale-105 hover:text-cyan-400 transition-all'>Blogs</button>
-      <button onClick={() => {
-        document.querySelector('#contact')?.scrollIntoView({ behavior: "smooth" })
-      }}
-        className='cursor-pointer hover:scale-105 hover:text-cyan-400 transition-all'>Contact</button>
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className='lg:text-lg xl:text-xl hidden md:flex fixed right-5 top-1/2 -translate-y-1/2 flex-col'
+    >
+      {links.map((item, idx) => (
+        <motion.button // Wrap the button with motion.button
+          key={idx}
+          variants={itemVariants} // Apply the item variants
+          onMouseEnter={() => setHovered(idx)}
+          onMouseLeave={() => setHovered(null)}
+          className="relative group py-4"
+          onClick={() => {
+            document.querySelector(item.href)?.scrollIntoView({ behavior: "smooth" })
+          }}
+        >
+          <span
+            className='px-2 cursor-pointer relative group-hover:text-white z-10 mx-auto'
+          >
+            {item.text}
+          </span>
+          {hovered == idx && (
+            <motion.span
+              layoutId="hover"
+              className="absolute bg-cyan-600 inset-0 rounded-full"
+            ></motion.span>
+          )}
+        </motion.button>
+      ))}
     </motion.nav>
   )
 }
 
 export default NavBar
-
